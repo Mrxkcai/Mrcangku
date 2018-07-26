58438db6cae2a5bfe691ecce0a196a5edfab88f6
xiekui <1073650251@qq.com>
2018-07-26 09:07:49 +0800

    整理文件结构

---
+++ assets/Views/Component/js/count.js
@@ -0,0 +1,185 @@
+Vue.component('count-block',{
+	props:{
+		countblock:{
+			type:Object,
+            default: {}
+		}
+	},
+	data(){
+		return{
+			
+		}
+	},
+	computed:{
+		
+	},
+	methods:{
+		goOrder(){
+			var urll = window.location.href;
+			if(urll.indexOf('index') == -1){
+				window.location.href = "../YuyueRepair/reservationRepair.html";
+			}else{
+				window.location.href = "Views/YuyueRepair/reservationRepair.html";
+			}
+			
+		},
+		
+		childs(){
+			var data = {
+					user_id:app.getItem('userInfo').id,		//	app.getItem('open_id')
+					pageNum:1,
+					pageSize:1,
+					seconds:api.pzTime
+				};
+				
+				$.ajax({
+					url: api.NWBDApiWeiXinpushOrder,			//	因为检查订单状态没有返回剩余时间，所以再次请求此接口
+					type: "POST",
+					data:data,
+					dataType: 'json',
+					success:function(result){
+//						console.log(result)
+						if(result.status == 'success' && result.code == 0){
+							app.closeLoading();
+							if(result.data.length > 0){
+								localStorage.setItem('status',1)
+								var countdown = result.data[0].orderInfo.countdown;
+								console.log(api.pzTime - countdown+"计时器")
+								localStorage.setItem('num',api.pzTime - countdown)
+								localStorage.setItem('orderId',result.data[0].orderInfo.id)
+								
+								//	查询订单状态
+								var data = {
+									order_id:result.data[0].orderInfo.id,
+									user_id:app.getItem('userInfo').id		//	app.getItem('open_id')
+								}
+						   		if(data.order_id){
+							   		var status = setInterval(function(){
+							   			 console.log('调用成功')
+							   			if(data.order_id != '' && data.user_id != ''){
+							   				$.ajax({
+								   				type:"POST",
+								   				url:api.NWBDApiWeiXincheckOrderStatus,
+								   				data:data,
+												dataType: 'json',
+												success:function(result){
+													 console.log(result)
+													if(result.code == 0){
+														if(result.data == 1){
+															app.alert('商家已接单')
+															localStorage.removeItem('status');
+															localStorage.removeItem('num');
+															clearInterval(status)
+															$('.countDown_box').hide();
+															$('.consumer_hotline ').removeClass('bottomP');
+															//	定位出现
+															$('.dwopa').show();		
+														}else if(result.data == 0){
+															//	未接单存1
+															localStorage.setItem('status',1)
+														}else if(result.data == 2){
+															app.alert('维修完成')
+															clearInterval(status)
+															localStorage.removeItem('status');
+															localStorage.removeItem('num');
+															$('.countDown_box').hide();
+															$('.consumer_hotline ').removeClass('bottomP');
+															//	定位出现
+															$('.dwopa').show();
+														}else if(result.data == 3){
+															app.alert('订单已取消')
+															clearInterval(status)
+															localStorage.removeItem('status');
+															localStorage.removeItem('num');
+															$('.countDown_box').hide();
+															$('.consumer_hotline ').removeClass('bottomP');
+															//	定位出现
+															$('.dwopa').show();
+														}else if(result.data == 4){
+															app.alert('订单超时未接收')
+															clearInterval(status)
+															localStorage.removeItem('status');
+															localStorage.removeItem('num');
+															$('.countDown_box').hide();
+															$('.consumer_hotline ').removeClass('bottomP');
+															//	定位出现
+															$('.dwopa').show();
+														}else if(result.data == 5){
+															app.alert('维修完成')
+															clearInterval(status)
+															localStorage.removeItem('status');
+															localStorage.removeItem('num');
+															$('.countDown_box').hide();
+															$('.consumer_hotline ').removeClass('bottomP');
+															//	定位出现
+															$('.dwopa').show();
+														}else{
+															
+														}
+														//	待确认跳转
+						//									window.location.href="../QuickRepair/QuickRepair.html"
+													}else{
+														clearInterval(status)
+														localStorage.removeItem('status');
+														localStorage.removeItem('num');
+														$('.countDown_box').hide();
+														$('.consumer_hotline ').removeClass('bottomP');
+														//	定位出现
+														$('.dwopa').show();
+													}
+												},
+												error:function(){
+													alert('操作失败，请检查网络！');
+									                app.closeLoading();
+									                clearInterval(status)
+												}
+								   			});
+							   			}
+							   			
+							   		},2000)
+							   	}
+							}else{
+								$('.consumer_hotline ').removeClass('bottomP')
+								localStorage.removeItem('status');
+								localStorage.removeItem('num');
+								//	定位出现
+								$('.dwopa').show();
+							}
+							
+						}
+						app.closeLoading();
+					},
+					error: function () {
+//		                alert('操作失败，请检查网络！');
+		                app.closeLoading();
+		          }
+					
+				});
+				
+		},
+		
+		
+	},
+	template:`
+		<div class="countDown_box">
+    		<div class="count_timer">
+	    		<span class="count_number count_number1">正在接单中</span>
+	    		<div class="count_number count_number2">
+	    			<span>
+	    				{{countblock.min}}
+	    			</span>
+	    			:
+	    			<span>
+	    				{{countblock.sec}}
+	    			</span>
+	    		</div>
+	    	</div>
+	    	
+	    	<div class="count_btn" @click="goOrder()">
+	    		查看
+	    	</div>
+	    	
+	    	<p class="count_seconds">15分钟内若商家未接单，系统将自动取消订单</p>
+    </div>
+	`
+});
---
+++ assets/js/app.js
@@ -0,0 +1,155 @@
+(function ($, app) {
+        "use strict";
+        FastClick.attach(window.document.body);
+        Date.prototype.toLocaleString = function () {
+            return this.getFullYear() + "年" + (this.getMonth() + 1) + "月" + this.getDate() + "日 " + this.getHours() + ":" + this.getMinutes() + ":" + (this.getSeconds() <= 9 ? "0" + this.getSeconds() : this.getSeconds());
+        };
+
+        app.options = {};
+
+        app.setItem = function (key, val) {
+            window.localStorage.setItem(key, JSON.stringify(val));
+        };
+        app.getItem = function (key) {
+            if (!window.localStorage.getItem(key) || window.localStorage.getItem(key) === "undefined" || window.localStorage.getItem(key) === null || window.localStorage.getItem(key) === undefined) {
+                return null;
+            } else {
+                return JSON.parse(window.localStorage.getItem(key));
+            }
+        };
+        app.removeItem = function (key) {
+            window.localStorage.removeItem(key);
+        };
+
+        app.alert = function (str) {
+            layer.open({
+                content: str,
+                style: 'background:rgba(0,0,0,.4); color:#fff; border:none;font-size:0.28rem;',
+                time: 1.5,
+                skin: 'msg'
+            });
+        };
+        app.loading = function () {
+            layer.open({
+                type: 2,
+                anim: "up",
+                shadeClose: false,
+                success: function () {
+
+                }
+            });
+        };
+        app.closeLoading = function () {
+            layer.closeAll();
+        };
+
+        app.getQueryString = function (name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) return unescape(r[2]);
+            return null;
+        };
+
+        app.f_close = function () {
+            if (typeof(WeixinJSBridge) != "undefined") {
+                WeixinJSBridge.call('closeWindow');
+            } else {
+                window.opener = null;
+                window.open('', '_self', '');
+                window.close();
+            }
+        };
+
+        app.verificationUserInfo = function () {
+            if (!app.getItem("userInfo") || !app.getItem("userInfo").id || !app.getItem("userInfo").mobile || !app.getItem("open_id")) {
+                alert("您还没有登录！");
+                if (api.isDebug) {
+                    app.setItem("open_id", "oalBd0epVVUS-w1rswxpJsaj2Fqc");
+                    window.location.href = api.getLocalhostPaht() + "/" + api.debugProjectName + "/index.html";
+                } else {
+                    window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + api.appid + "&redirect_uri=https://" + api.callbackUrl + "/index.html&response_type=code&scope=snsapi_base&state=STATE&connect_redirect=1#wechat_redirect";
+                }
+            }
+        };
+
+        //初始化代码
+        app.init = function (options) {
+            // $(window.document.body).css({'min-height': $(document).height() + 'px'});
+            options = options || {};
+            return $.extend(app.options, options);
+        };
+        
+        
+    
+		//	查询订单剩余时间
+		app.checkTime = function () {
+			
+			var data = {
+				user_id:app.getItem('userInfo').id,		//	app.getItem('open_id')
+				pageNum:1,
+				pageSize:1,
+				seconds:api.pzTime
+			};
+			var tt;
+			$.ajax({
+				url: api.NWBDApiWeiXinpushOrder,
+				type: "POST",
+				data:data,
+				dataType: 'json',
+				async:false,			//	同步的使用
+				success:function(result){
+//					console.log(result)
+					if(result.status == 'success' && result.code == 0){
+						
+						if(result.data.length > 0){
+							var countd = result.data[0].orderInfo.countdown;
+							tt = api.pzTime - countd;
+							localStorage.setItem('status',1);
+						}else{
+							localStorage.removeItem('status');
+						}
+					}
+				},
+				error:function(){
+					
+				}
+			});
+			return tt;
+		};
+		
+		//	时间戳转化
+		app.getTime = function(createTime,a){
+			var date = new Date(createTime)
+			var Y, M, D, h, m, s
+			Y = date.getFullYear() + '年'
+			M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '月'
+			D = app.tranformTow(date.getDate())  + '日 '
+			h = app.tranformTow(date.getHours()) + ':'
+			m = app.tranformTow(date.getMinutes())
+		    s = app.tranformTow(date.getSeconds())
+		    
+		    if(a == 1){
+		        return Y + M + D
+		    }else if(a == 2){
+		        return Y + M + D + h + m 
+		    }else if(a == 3){
+		        return Y + M + D + h + m  + ':' + s
+		    }
+		};
+		
+		//	时间戳中个位数转化十位数
+		app.tranformTow = function (number){
+			return number < 10 ? '0'+number:number
+		};
+		
+		app.layerAlert = function (str) {
+	        layer.open({
+	            content: str,
+	            style: 'background:rgba(0,0,0,.7); color:#fff; border:none;font-size:0.28rem;',
+	            time: 2,
+	            skin: 'msg'
+	        });
+	    };
+
+	}
+)($, window.app = {});
