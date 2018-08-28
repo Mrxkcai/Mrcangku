a00759cbca621887b5356d23f977a3cf762e5591
EDZ <edz@EDZdeMacBook-Pro-3.local>
2018-08-28 14:57:28 +0800

    test

--- assets/js/config.js
+++ assets/js/config.js
@@ -70,7 +70,7 @@ var api = (function () {
         //document.write("<script src='" + api.getLocalhostPaht() + "/js/vue-lazyload.js?v=1.0.4' charset='utf-8'></script>");       //  懒加载js
         document.write("<script src='" + api.getLocalhostPaht() + "/js/app.js?v=2.0.0' charset='utf-8'></script>");       ///bbcf-common-h5/assets
         document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/notives.js?v=2.0.5' charset='utf-8'></script>");     //  活动js
-        document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/count.js?v=2.0.2' charset='utf-8'></script>");       //  计时器js
+        document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/count.js?v=2.0.3' charset='utf-8'></script>");       //  计时器js
         document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/guide.js?v=2.0.0' charset='utf-8'></script>");       //  指南js
         
         //  引入活动css
