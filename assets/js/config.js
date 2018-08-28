5037cc58b62c092d4195569098e307f9d995dd49
EDZ <edz@EDZdeMacBook-Pro-3.local>
2018-08-28 17:50:12 +0800

    修改引入webapijs

--- assets/js/config.js
+++ assets/js/config.js
@@ -72,7 +72,8 @@ var api = (function () {
         document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/notives.js?v=2.0.5' charset='utf-8'></script>");     //  活动js
         document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/count.js?v=2.0.4' charset='utf-8'></script>");       //  计时器js
         document.write("<script src='" + api.getLocalhostPaht() + "/Views/Component/js/guide.js?v=2.0.0' charset='utf-8'></script>");       //  指南js
-        
+        document.write('<script src="https://webapi.amap.com/maps?v=1.4.7&key=e9d83bcf337ca24921e9af7aee928b4d"></script>')                 //  高德地图api
+
         //  引入活动css
         $("<link>").attr({ rel: "stylesheet",
                 type: "text/css",
